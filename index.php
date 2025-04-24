4b68281e54502a001ec8fa12083261681aa1381b
Nuno <up202306826@up.pt>
2025-04-24 15:09:41 +0100

    Added the start of header and footer template

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include_once('templates/common.tpl.php'); drawHeader(); ?>
+
+<header>
+  <h1>Freely</h1>
+  <nav>
+    <a href="login.php">Login</a>
+  </nav>
+</header>
+
+<main>
+  <p>Welcome to the FreeLance project!</p>
+</main>
+
+<?php drawFooter(); ?>
---
+++ templates/common.tpl.php
@@ -0,0 +1,23 @@
+<?php function drawHeader() { ?>
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title><?=htmlspecialchars($title)?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" 
+            integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" 
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <script src="../javascript/script.js" defer></script>
+    </head>
+    <body>
+
+<?php } function drawFooter() { ?>
+    <footer>
+        LTW Freelancer website &copy; 2025
+    </footer>    
+    </body>
+    </html>
+<?php } ?>
