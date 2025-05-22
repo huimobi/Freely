a070cd7e500d62e0c747dcb5d38bf6da978bf93e
-v <up202306826@up.pt>
2025-05-22 22:18:23 +0100

    Minor changes

--- templates/service_card.tpl.php
+++ templates/service_card.tpl.php
@@ -1,11 +1,17 @@
 <?php
 declare(strict_types=1);
 
-function drawServiceCard($svc): void { ?>
+function drawServiceCard($svc): void { 
+    $serviceImgRel  = "/images/services/{$svc->id}/0.jpg";
+    $serviceImgAbs  = $_SERVER['DOCUMENT_ROOT'] . $serviceImgRel;
+    $sellerImgRel   = "/images/users/{$svc->seller->id}.jpg";
+    $sellerImgAbs   = $_SERVER['DOCUMENT_ROOT'] . $sellerImgRel;
+    if (!file_exists($serviceImgAbs)) {$serviceImgRel = "/images/services/default.jpg";}
+    if (!file_exists($sellerImgAbs)) {$sellerImgRel = "/images/users/default.jpg";}
+  ?>
   <a href="/pages/service.php?id=<?= $svc->id ?>" class="service-card-link">
     <article class="service-card">
-      <img src="/images/services/<?= $svc->id ?>/0.jpg" class="service-img"
-          onerror="this.src='/images/services/default.jpg'" alt="Service">
+      <img src="<?= $serviceImgRel ?>" class="service-img" alt="Service">
 
       <div class="service-info">
         <h3 class="service-title"><?= htmlspecialchars($svc->title) ?></h1>
@@ -17,7 +23,7 @@ function drawServiceCard($svc): void { ?>
       </div>
 
       <div class="service-seller">
-        <img src="/images/users/<?= $svc->seller->id ?>.jpg" class="seller-img" onerror="this.src='/images/users/default.jpg'" alt="Seller">
+        <img src="<?= $sellerImgRel ?>" class="seller-img"  alt="Seller">
         <div class="seller-details">
           <span class="seller-name"><?= htmlspecialchars($svc->seller->firstName . ' ' . $svc->seller->lastName) ?></span>
           <span class="seller-rating"> ⭐ <?= $svc->rating ?? '4.9' ?> (<?= $svc->numRatings ?? '100' ?>) </span>
